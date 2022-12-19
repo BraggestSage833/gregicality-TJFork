a34f7aaa6a478a64ddf1238b4981e197c2dad82a
James <39562198+YoungOnionMC@users.noreply.github.com>
2022-12-19 03:36:58 -0700

    more stoich fixes and version bump

--- src/main/java/gregicadditions/recipes/chain/SeleniumChain.java
+++ src/main/java/gregicadditions/recipes/chain/SeleniumChain.java
@@ -59,11 +59,11 @@ public class SeleniumChain {
                 .fluidInputs(SeleniteTelluriteMix.getFluid(1000))
                 .fluidInputs(SulfuricAcid.getFluid(1000))
                 .outputs(TelluriumOxide.getItemStack(3))
-                .output(dust, Sodium, 4)
                 .fluidOutputs(Water.getFluid(2000))
-                .fluidOutputs(SulfurDioxide.getFluid(1000))
-                .fluidOutputs(CarbonMonoxde.getFluid(1000))
+                .fluidOutputs(SulfurTrioxide.getFluid(1000))
+                .fluidOutputs(CarbonDioxide.getFluid(1000))
                 .fluidOutputs(SeleniteSolution.getFluid(1000))
+                .fluidOutputs(Water.getFluid(1000))
                 .buildAndRegister();
 
         // Na2SeO3 + 2HCl -> 2NaCl + SeO2 + H2O
